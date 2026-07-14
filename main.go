93f0432c1b610975194019518ea401b1c575ea1c
kenzierivan <kenzierivan261@gmail.com>
2026-07-14 19:31:13 +0800

    chore: add comments for 8XY_ instructions

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ type Chip8 struct {
 	soundTimer uint8
 	v          [16]byte
 }
+
 var font = [80]byte{
 	0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
 	0x20, 0x60, 0x20, 0x20, 0x70, // 1
@@ -28,6 +29,7 @@ var font = [80]byte{
 	0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
 	0xF0, 0x80, 0xF0, 0x80, 0x80, // F
 }
+
 const fontStartAddr = 0x050
 
 func main() {
@@ -35,7 +37,7 @@ func main() {
 
 	// Load font to memory 050-09F (popular convention)
 	for i, char := range font {
-		c.memory[fontStartAddr + i] = char
+		c.memory[fontStartAddr+i] = char
 	}
 }
 
@@ -49,7 +51,7 @@ func NewChip8() *Chip8 {
 
 func (c *Chip8) Cycle() {
 	// Shift 8 bits and do OR operation to combine to a full instruction
-	opcode := uint16(c.memory[c.pc]) << 8 | uint16(c.memory[c.pc + 1])
+	opcode := uint16(c.memory[c.pc])<<8 | uint16(c.memory[c.pc+1])
 	c.pc += 2
 
 	op := (opcode & 0xF000) >> 12
@@ -59,13 +61,13 @@ func (c *Chip8) Cycle() {
 	nn := opcode & 0x00FF
 	nnn := opcode & 0x0FFF
 	switch op {
-	case 0x0: 
+	case 0x0:
 		switch nnn {
 		case 0x0E0: // 00E0 - CLS: clear display
 			c.display = [32][64]bool{}
 		case 0x0EE: // 00EE - RET: return from subroutine
-			c.pc = c.stack[len(c.stack) - 1]
-			c.stack = c.stack[:len(c.stack) - 1]
+			c.pc = c.stack[len(c.stack)-1]
+			c.stack = c.stack[:len(c.stack)-1]
 		}
 	case 0x1: // 1NNN - JP addr: jump to NNN
 		c.pc = nnn
@@ -90,15 +92,15 @@ func (c *Chip8) Cycle() {
 		c.v[x] += byte(nn)
 	case 0x8:
 		switch n {
-		case 0x0: // LD Vx, byte - 
+		case 0x0: // 8XY0 - LD Vx, Vy: set VX to VY
 			c.v[x] = c.v[y]
-		case 0x1:
+		case 0x1: // 8XY1 - OR Vx, Vy: set VX to VX OR VY
 			c.v[x] = c.v[x] | c.v[y]
-		case 0x2:
+		case 0x2: // 8XY2 - AND Vx, Vy: set VX to VX AND VY
 			c.v[x] = c.v[x] & c.v[y]
-		case 0x3:
+		case 0x3: // 8XY3 - XOR Vx, Vy: set VX to VX XOR VY
 			c.v[x] = c.v[x] ^ c.v[y]
-		case 0x4:
+		case 0x4: // 8XY4 - ADD Vx, Vy: VX += VY, VF set to 1 on overflow, else 0
 			sum := uint16(c.v[x]) + uint16(c.v[y])
 			if sum > 0xFF {
 				c.v[0xF] = 1
@@ -106,25 +108,25 @@ func (c *Chip8) Cycle() {
 				c.v[0xF] = 0
 			}
 			c.v[x] = byte(sum)
-		case 0x5:
+		case 0x5: // 8XY5 - SUB Vx, Vy: VX -= VY, VF set to 1 if no borrow (VX >= VY), else 0
 			if c.v[x] >= c.v[y] {
 				c.v[0xF] = 1
 			} else {
 				c.v[0xF] = 0
 			}
 			c.v[x] = c.v[x] - c.v[y]
-		case 0x6: 
+		case 0x6: // 8XY6 - SHR Vx: shift VX right 1 bit, VF set to bit shifted out
 			shiftedBit := c.v[x] & 0x1
 			c.v[x] = c.v[x] >> 1
 			c.v[0xF] = shiftedBit
-		case 0x7:
+		case 0x7: // 8XY7 - SUB Vx, Vy: VX = VY - VX, VF set to 1 if no borrow (VY >= VX), else 0
 			if c.v[y] >= c.v[x] {
 				c.v[0xF] = 1
 			} else {
 				c.v[0xF] = 0
 			}
 			c.v[x] = c.v[y] - c.v[x]
-		case 0xE:
+		case 0xE: // 8XYE - SHL Vx: shift VX left 1 bit, VF set to bit shifted out
 			shiftedBit := (c.v[x] & 0x80) >> 7
 			c.v[x] = c.v[x] << 1
 			c.v[0xF] = shiftedBit
@@ -140,13 +142,13 @@ func (c *Chip8) Cycle() {
 		screenY := uint16(c.v[y] & 31)
 		c.v[0xF] = 0
 		for row := 0; row < int(n); row++ {
-			spriteByte := c.memory[c.i + uint16(row)]
+			spriteByte := c.memory[c.i+uint16(row)]
 			pixelY := screenY + uint16(row)
 			if pixelY > 31 {
 				break
 			}
 			for col := range 8 {
-				pixelX :=  screenX + uint16(col)
+				pixelX := screenX + uint16(col)
 				spriteBit := spriteByte & (0x80 >> col)
 				if pixelX > 63 {
 					break
@@ -157,8 +159,8 @@ func (c *Chip8) Cycle() {
 				} else if spriteBit != 0 && c.display[pixelY][pixelX] == false {
 					c.display[pixelY][pixelX] = true
 				}
-				
+
 			}
 		}
-	}	
+	}
 }
